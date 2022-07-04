f0dc1468f4f3731459976ffb92f590bcee2fc809
muditgarg48 <muditgarg48@gmail.com>
2022-07-04 13:30:05 +0530

    Code cleanup

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
-//Packages
 import "package:flutter/material.dart";
 
-//Variables
 import "../global_values.dart";
 
 class MyHomePage extends StatefulWidget {
@@ -12,53 +10,6 @@ class MyHomePage extends StatefulWidget {
 
 class MyHomePageState extends State<MyHomePage> {
   int currentPageIndex = 0;
-
-  // void switchTheme(String choice) {
-  //   setState(() {
-  //     if (themeMode == ThemeMode.light) {
-  //       themeMode = ThemeMode.dark;
-  //       themeIcon = Icons.sunny;
-  //       isDark = true;
-  //     } else if (themeMode == ThemeMode.dark) {
-  //       themeMode = ThemeMode.light;
-  //       themeIcon = Icons.dark_mode;
-  //       isDark = false;
-  //     }
-  //   });
-  //   (context as Element).reassemble();
-  //   // displayTheme();
-  // }
-
-  // FloatingActionButton themeButton() {
-  //   return FloatingActionButton.extended(
-  //     backgroundColor: invertedThemeTxtColor(),
-  //     onPressed: () {
-  //       var warning = WarningDialogue(
-  //         confirmationMsg: "Toggle light/dark mode ?",
-  //         choice: "theme_change",
-  //         jobDone: Icons.toggle_on,
-  //         action: switchTheme,
-  //       );
-  //       warning.dialogueBox(context);
-  //     },
-  //     tooltip: "Choose theme",
-  //     label: Row(
-  //       children: [
-  //         Icon(
-  //           themeIcon,
-  //           color: themeBgColor(),
-  //         ),
-  //         const SizedBox(width: 5),
-  //         QudsAnimatedText(
-  //           !isDark ? "DARK" : "LIGHT",
-  //           style: TextStyle(
-  //             color: themeBgColor(),
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 
   // void displayTheme() {
   //   print(themeMode);
@@ -80,13 +31,6 @@ class MyHomePageState extends State<MyHomePage> {
         index: currentPageIndex,
         children: <Widget>[...pages.map((page) => page["class"]).toList()],
       ),
-      // bottomNavigationBar: MyBottomNavbar1(
-      //   pages: pages,
-      //   currentPageIndex: currentPageIndex,
-      //   setPageIndex: setPageIndex,
-      // ),
-      // floatingActionButton: themeButton(),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
