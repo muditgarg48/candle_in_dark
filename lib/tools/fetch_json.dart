56b5d212dd56e3a755d2a859f220cee8735a4b59
muditgarg48 <muditgarg48@gmail.com>
2022-07-08 21:15:11 +0530

    Changing the return type of JSON data function from dynamic to Future<dynamic> for better understanding

--- lib/tools/fetch_json.dart
+++ lib/tools/fetch_json.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 
 //from link
 // ignore: non_constant_identifier_names
-dynamic fetchFromJSON_Online(String link) async {
+Future fetchFromJSON_Online(String link) async {
   Response response = await get(Uri.parse(link));
   var data = jsonDecode(response.body);
   return data;
@@ -14,7 +14,7 @@ dynamic fetchFromJSON_Online(String link) async {
 
 //from local
 // ignore: non_constant_identifier_names
-dynamic fetchFromJSON_Local(String link) async {
+Future fetchFromJSON_Local(String link) async {
   var file = await rootBundle.loadString(link);
   var data = jsonDecode(file);
   return data;
